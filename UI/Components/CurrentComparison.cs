3acb7b5c089f123540bf4ef00c965b48f674d2a9
CryZe <cryze92@gmail.com>
2014-07-13 02:11:37 +0200

    Initial Commit

---
+++ UI/Components/CurrentComparison.cs
@@ -0,0 +1,150 @@
+﻿using LiveSplit.Model;
+using LiveSplit.Model.Comparisons;
+using LiveSplit.TimeFormatters;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveSplit.UI.Components
+{
+    public class CurrentComparison : IComponent
+    {
+        protected InfoTextComponent InternalComponent { get; set; }
+        public CurrentComparisonSettings Settings { get; set; }
+
+        public GraphicsCache Cache { get; set; }
+
+        public float PaddingTop { get { return InternalComponent.PaddingTop; } }
+        public float PaddingLeft { get { return InternalComponent.PaddingLeft; } }
+        public float PaddingBottom { get { return InternalComponent.PaddingBottom; } }
+        public float PaddingRight { get { return InternalComponent.PaddingRight; } }
+
+        public IDictionary<string, Action> ContextMenuControls
+        {
+            get { return null; }
+        }
+
+        public CurrentComparison(LiveSplitState state)
+        {
+            Cache = new GraphicsCache();
+            Settings = new CurrentComparisonSettings()
+            {
+                CurrentState = state
+            };
+            InternalComponent = new InfoTextComponent("Comparing Against", "");
+        }
+
+        private void PrepareDraw(LiveSplitState state, LayoutMode mode)
+        {
+            InternalComponent.DisplayTwoRows = Settings.Display2Rows;
+
+            InternalComponent.NameLabel.HasShadow 
+                = InternalComponent.ValueLabel.HasShadow
+                = state.LayoutSettings.DropShadows;
+
+            InternalComponent.ValueLabel.Font = state.LayoutSettings.TextFont;
+            InternalComponent.NameLabel.ForeColor = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
+            InternalComponent.ValueLabel.ForeColor = Settings.OverrideTimeColor ? Settings.TimeColor : state.LayoutSettings.TextColor;
+        }
+
+        private void DrawBackground(Graphics g, LiveSplitState state, float width, float height)
+        {
+            if (Settings.BackgroundColor.ToArgb() != Color.Transparent.ToArgb()
+                || Settings.BackgroundGradient != GradientType.Plain
+                && Settings.BackgroundColor2.ToArgb() != Color.Transparent.ToArgb())
+            {
+                var gradientBrush = new LinearGradientBrush(
+                            new PointF(0, 0),
+                            Settings.BackgroundGradient == GradientType.Horizontal
+                            ? new PointF(width, 0)
+                            : new PointF(0, height),
+                            Settings.BackgroundColor,
+                            Settings.BackgroundGradient == GradientType.Plain
+                            ? Settings.BackgroundColor
+                            : Settings.BackgroundColor2);
+                g.FillRectangle(gradientBrush, 0, 0, width, height);
+            }
+        }
+
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+        {
+            DrawBackground(g, state, width, VerticalHeight);
+            PrepareDraw(state, LayoutMode.Vertical);
+            InternalComponent.DrawVertical(g, state, width, clipRegion);
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+        {
+            DrawBackground(g, state, HorizontalWidth, height);
+            PrepareDraw(state, LayoutMode.Horizontal);
+            InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+        }
+
+        public float VerticalHeight
+        {
+            get { return InternalComponent.VerticalHeight; }
+        }
+
+        public float MinimumWidth
+        {
+            get { return InternalComponent.MinimumWidth; }
+        }
+
+        public float HorizontalWidth
+        {
+            get { return InternalComponent.HorizontalWidth; }
+        }
+
+        public float MinimumHeight
+        {
+            get { return InternalComponent.MinimumHeight; }
+        }
+
+        public string ComponentName
+        {
+            get { return "Current Comparison"; }
+        }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            Settings.Mode = mode;
+            return Settings;
+        }
+
+        public void SetSettings(System.Xml.XmlNode settings)
+        {
+            Settings.SetSettings(settings);
+        }
+
+        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return Settings.GetSettings(document);
+        }
+
+
+        public void RenameComparison(string oldName, string newName)
+        {
+        }
+
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            InternalComponent.LongestString = InternalComponent.NameLabel.Text;
+            InternalComponent.ValueLabel.Text = state.CurrentComparison;
+
+            Cache.Restart();
+            Cache["Comparison"] = InternalComponent.ValueLabel.Text;
+
+            if (invalidator != null && Cache.HasChanged)
+            {
+                invalidator.Invalidate(0, 0, width, height);
+            }
+        }
+    }
+}
