6e9b8ee37e1ed4b6deed7fa9f85237a4f772b96b
Ero <imjustero@gmail.com>
2024-09-02 10:07:07 +0200

    Use file-scoped namespaces, move `using` directives, fix whitespace
    * `IDE0005`, `IDE0065`, `IDE0161`

--- src/LiveSplit.CurrentComparison/UI/Components/CurrentComparisonFactory.cs
+++ src/LiveSplit.CurrentComparison/UI/Components/CurrentComparisonFactory.cs
@@ -1,27 +1,27 @@
-﻿using LiveSplit.Model;
+﻿using System;
+
+using LiveSplit.Model;
 using LiveSplit.UI.Components;
-using System;
 
 [assembly: ComponentFactory(typeof(CurrentComparisonFactory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class CurrentComparisonFactory : IComponentFactory
 {
-    public class CurrentComparisonFactory : IComponentFactory
-    {
-        public string ComponentName => "Current Comparison";
+    public string ComponentName => "Current Comparison";
 
-        public string Description => "Shows which comparison you are currently comparing to.";
+    public string Description => "Shows which comparison you are currently comparing to.";
 
-        public ComponentCategory Category => ComponentCategory.Information;
+    public ComponentCategory Category => ComponentCategory.Information;
 
-        public IComponent Create(LiveSplitState state) => new CurrentComparison(state);
+    public IComponent Create(LiveSplitState state) => new CurrentComparison(state);
 
-        public string UpdateName => ComponentName;
+    public string UpdateName => ComponentName;
 
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.CurrentComparison.xml";
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.CurrentComparison.xml";
 
-        public string UpdateURL => "http://livesplit.org/update/";
+    public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.29");
-    }
+    public Version Version => Version.Parse("1.8.29");
 }
