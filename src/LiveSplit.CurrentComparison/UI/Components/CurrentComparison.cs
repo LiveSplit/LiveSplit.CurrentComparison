b29255adf24211ec04f6c9df02197b06c495f26f
wooferzfg <spapushin@gmail.com>
2026-04-06 00:05:52 -0400

    Revert "Localize CurrentComparison component labels (#10)"
    This reverts commit 7ea619f6f33a59794e03e0c76f54ae2449607121.

--- src/LiveSplit.CurrentComparison/UI/Components/CurrentComparison.cs
+++ src/LiveSplit.CurrentComparison/UI/Components/CurrentComparison.cs
@@ -4,15 +4,12 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
-using LiveSplit.Localization;
 using LiveSplit.Model;
 
 namespace LiveSplit.UI.Components;
 
 public class CurrentComparison : IComponent
 {
-    private static string T(string source) => UiLocalizer.Translate(source, LanguageResolver.ResolveCurrentCultureLanguage());
-
     protected InfoTextComponent InternalComponent { get; set; }
     public CurrentComparisonSettings Settings { get; set; }
 
@@ -29,12 +26,12 @@ public class CurrentComparison : IComponent
         {
             CurrentState = state
         };
-        InternalComponent = new InfoTextComponent(T("Comparing Against"), "")
+        InternalComponent = new InfoTextComponent("Comparing Against", "")
         {
             AlternateNameText = new[]
             {
-                T("Comparison"),
-                T("Comp.")
+                "Comparison",
+                "Comp."
             }
         };
     }
@@ -92,7 +89,7 @@ public class CurrentComparison : IComponent
 
     public float MinimumHeight => InternalComponent.MinimumHeight;
 
-    public string ComponentName => T("Current Comparison");
+    public string ComponentName => "Current Comparison";
 
     public Control GetSettingsControl(LayoutMode mode)
     {
